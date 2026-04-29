9065c0bf4f7a37cfbca39ed48353a81ac18023ee
Pavel Sokolov <pavel.sok77@gmail.com>
2026-04-29 15:16:04 +0300

    first part, organism and pages

---
+++ poms/pages/ToDoPage.ts
@@ -0,0 +1,52 @@
+import { expect, Locator, Page } from "@playwright/test";
+import { ToDoItem } from "../organisms/ToDoItem";
+import { faker } from "@faker-js/faker/locale/en";
+
+export class ToDoPage {
+  readonly page: Page;
+  private readonly url = "https://todo-app.tallinn-learning.ee/";
+  readonly header: Locator;
+  readonly main: Locator;
+  readonly footer: Locator;
+  readonly todoItemInput: Locator;
+  //readonly externalFooter: Locator
+
+  constructor(page: Page) {
+    this.page = page;
+    this.header = page.getByTestId("header");
+    this.main = page.getByTestId("main");
+    this.footer = page.getByTestId("footer");
+    this.todoItemInput = page.getByTestId("text-input");
+  }
+
+  getToDoItemByIndex(index: number): ToDoItem {
+    return new ToDoItem(this.main.getByTestId("todo-item").nth(index));
+  }
+
+  getToDoItemByText(text: string): ToDoItem {
+    return new ToDoItem(
+      this.main.locator('[data-testid="todo-item"]', { hasText: text }),
+    );
+  }
+
+  async goto(): Promise<void> {
+    await this.page.goto(this.url);
+  }
+
+  async createToDoItem(text?: string): Promise<ToDoItem> {
+    await this.todoItemInput.fill(
+      text == undefined ? faker.word.words(2) : text,
+    );
+    await this.todoItemInput.press("Enter");
+
+    const todoItems = this.main.getByTestId("todo-item");
+    const itemCount = await todoItems.count();
+    return this.getToDoItemByIndex(itemCount - 1);
+  }
+
+  async checkToDoItemsVisible(expectedCount: number): Promise<void> {
+    //const todoItems: ToDoItem[] = this.main.getByTestId('todo-item');
+    const itemCount: number = await this.main.getByTestId("todo-item").count();
+    expect(itemCount).toBe(expectedCount);
+  }
+}
---
+++ tests/todoAppTests.spec.ts
@@ -0,0 +1,49 @@
+import { test, expect } from "@playwright/test";
+import { ToDoPage } from "../poms/pages/ToDoPage";
+
+test("Create to-do item", async ({ page }) => {
+  const toDoPage = new ToDoPage(page);
+  await toDoPage.goto();
+  await toDoPage.checkToDoItemsVisible(0);
+  await toDoPage.createToDoItem();
+  await toDoPage.checkToDoItemsVisible(1);
+});
+
+test("Create 2 to-do item", async ({ page }) => {
+  const toDoPage = new ToDoPage(page);
+  await toDoPage.goto();
+  await toDoPage.createToDoItem();
+  await toDoPage.createToDoItem();
+  await toDoPage.checkToDoItemsVisible(2);
+});
+
+test("Activate card test", async ({ page }) => {
+  const toDoPage = new ToDoPage(page);
+  await toDoPage.goto();
+  const createdToDo = await toDoPage.createToDoItem();
+  await toDoPage.checkToDoItemsVisible(1);
+  await createdToDo.activate();
+  await createdToDo.checkIsActivated();
+});
+
+test("Activate card test - search by text", async ({ page }) => {
+  const cardText = "test text";
+  const toDoPage = new ToDoPage(page);
+  await toDoPage.goto();
+  await toDoPage.createToDoItem(cardText);
+  await toDoPage.checkToDoItemsVisible(1);
+  const createdToDo = toDoPage.getToDoItemByText(cardText);
+  await createdToDo.activate();
+  await createdToDo.checkIsActivated();
+});
+
+test("Delete card test", async ({ page }) => {
+  const toDoPage = new ToDoPage(page);
+  await toDoPage.goto();
+  const createdToDo = await toDoPage.createToDoItem();
+  await toDoPage.checkToDoItemsVisible(1);
+  await createdToDo.deleteItem();
+  await toDoPage.checkToDoItemsVisible(0);
+});
+
+//npx playwright test -g "Activate card test" --debug
